5d3c1d63170a9bf2e587f7bdc64b3511cf44bcfc
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2022-11-02 15:24:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ app.secret_key = "secret"
 csrf = CSRFProtect(app)
 
 
+# manejo de errores
+@app.errorhandler(404)
+def pageNotFound(e):
+    return redirect(url_for('inicio', id=1))
+
+
 #ruta inicial , SomeHose=
 @app.route("/")
 def init():
@@ -18,12 +24,12 @@ def init():
 
 @app.route("/inicio/<int:id>", methods=['GET', 'POST'])
 def inicio(id):
-    n = math.ceil(len(getHome()) / 20)
+    n = math.ceil(len(getHome()) / 15)
     homes = []
     if id == 1:
-        homes.append(getHome()[0:20])
+        homes.append(getHome()[0:15])
     elif id in range(1, n + 1) and id != 1:
-        homes.append(getHome()[20 * (id - 1):20 * id])
+        homes.append(getHome()[15 * (id - 1):15 * id])
 
     return render_template("index.html", Alloc=getAllLoc(), home=homes[0], n=n, id=id)
 
@@ -54,14 +60,14 @@ def Buscar(id):
     valor = request.form['valor']
     Cpersonas = request.form['Cpersonas']
     getSearchHome(Localidad, valor, Cpersonas)
-    n = math.ceil(len(getSearchHome(Localidad, valor, Cpersonas)) / 20)
+    n = math.ceil(len(getSearchHome(Localidad, valor, Cpersonas)) / 15)
     homes = []
     if id == 1:
-        homes.append(getSearchHome(Localidad, valor, Cpersonas)[0:20])
+        homes.append(getSearchHome(Localidad, valor, Cpersonas)[0:15])
     elif id in range(1, n + 1) and id != 1:
-        homes.append(getSearchHome(Localidad, valor, Cpersonas)[20 * (id - 1):20 * id])
+        homes.append(getSearchHome(Localidad, valor, Cpersonas)[15 * (id - 1):15 * id])
     return render_template("Busqueda.html", home=homes[0], n=n, id=id,
-                           nhome=bool(homes[0]), loc=Localidad, val=valor, cp=Cpersonas)
+                           nhome=bool(homes[0]), loc=Localidad, val=valor, cp=Cpersonas, Alloc=getAllLoc())
 
 
 @app.route("/Buscar/filtro/<int:id>", methods=['POST'])
@@ -72,23 +78,26 @@ def filtro(id):
     val = request.form['val']
     cp = request.form['cp']
     loc = request.form['loc']
-    n = math.ceil(len(getSearchHome(loc, val, cp)) / 20)
+    n = math.ceil(len(getSearchHome(loc, val, cp)) / 15)
     homes = []
     if id == 1:
-        homes.append(getSearchHome(loc, val, cp)[0:20])
+        homes.append(getSearchHome(loc, val, cp)[0:15])
     elif id in range(1, n + 1) and id != 1:
-        homes.append(getSearchHome(loc, val, cp)[20 * (id - 1):20 * id])
+        homes.append(getSearchHome(loc, val, cp)[15 * (id - 1):15 * id])
 
     comHome = []
     for i in homes[0]:
         if bool(getHomeComodidades(i[0], Dmar, mascota, parrilla)):
             comHome.append(getHomeCustom(i[0]))
 
-    return render_template("Busqueda.html", home=comHome[0], n=n, id=id,
-                           nhome=bool(comHome[0]), loc=loc, val=val, cp=cp,
-                           Alloc=getAllLoc())
-
-
+    if bool(comHome):
+        return render_template("Busqueda.html", home=comHome[0], n=n, id=id,
+                               nhome=bool(comHome[0]), loc=loc, val=val, cp=cp,
+                               Alloc=getAllLoc())
+    else:
+        return render_template("Busqueda.html",  n=n, id=id,
+                               nhome=False, loc=loc, val=val, cp=cp,
+                               Alloc=getAllLoc())
 
 
 
